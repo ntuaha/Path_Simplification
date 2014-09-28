3d3cd5afb382cb934bd608cf6140f083662ed1c0
Cheng-Yu Lin <ntuaha@gmail.com>
2014-09-28 14:59:35 +0800

    增加資料檔案下載

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ Path_Simplification
 ===================
 
 Simplify your path, curve, or curve under some specific criteria.
+
+## Data
+
+- 從 [https://www.cathaybk.com.tw/cathaybk/personal_info14_excel.asp](https://www.cathaybk.com.tw/cathaybk/personal_info14_excel.asp)利用POST傳送  
+
+	- SelCur=USD3%AC%FC%A4%B8&R1=2
+	- BDate=2014%2F06%2F27
+	- EDate=2014%2F09%2F27
+
+Example Code
+```{shell}
+curl -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31" --data "SelCur=USD3%AC%FC%A4%B8&R1=2&BDate=2014%2F01%2F01&EDate=2015%2F12%2F31" -o ../data/20140101_20151231.xls https://www.cathaybk.com.tw/cathaybk/personal_info14_excel.asp
+```
+
+可以直接執行,會自動下載從2006到現在的檔案
+
+```
+python getData.py [存放檔案路徑]
+```
+
---
+++ src/getData.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*- 
+
+import urllib2
+import urllib
+import time
+from datetime import date
+
+import os
+import sys
+
+
+# READ xlsx
+import xlrd
+import re
+
+#處理掉unicode 和 str 在ascii上的問題
+reload(sys) 
+sys.setdefaultencoding('utf8') 
+
+
+
+
+class CONTROLLER:
+	def run(self,parameters):
+		if len(parameters)<2:
+			raise "# of arguments is too few"
+		dl = DOWNLOAD(parameters[1])
+		for i  in xrange(2006,2016,2):
+			start_dt = str(i) + "0101"
+			end_dt = str(i+1) + "1231"
+			dl.download(start_dt,end_dt)
+
+
+
+class DOWNLOAD:
+	folder = None
+	start_dt = None
+	end_dt = None
+	def __init__(self,folder):
+		self.folder = folder
+		self.checkFolder()
+
+	def checkFolder(self):
+		if os.path.exists(self.folder) == False:
+			os.makedirs(self.folder)
+
+	def download(self,start_dt,end_dt):
+		self.start_dt = start_dt
+		self.end_dt = end_dt
+		server_path = "https://www.cathaybk.com.tw/cathaybk/personal_info14_excel.asp"
+		client_path = "%s/%s_%s.xls" % (self.folder,self.start_dt,self.end_dt)
+		agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31'
+		data = "SelCur=USD3%%AC%%FC%%A4%%B8&R1=2&BDate=%s%%2F%s%%2F%s&EDate=%s%%2F%s%%2F%s" % (self.start_dt[0:4],self.start_dt[4:6],self.start_dt[6:8],self.end_dt[0:4],self.end_dt[4:6],self.end_dt[6:8])
+		print server_path
+		print client_path
+		print data
+		cmd = 'curl -A "%s" --data "%s" -o %s %s'% (agent,data,client_path,server_path)
+		print cmd
+		os.system(cmd)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	CONTROLLER().run(sys.argv)
